4143b04ba4dc4d12a6ae1c5decb6bf606ad85b80
Priya Dutta <dutta.priya533@gmail.com>
2020-07-06 20:22:15 +0530

    New posts fetched after 10 seconds will be added to old posts.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function Item({ title }) {
   return (
-    <View>
-      <Text>{title}</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableOpacity style={{ flex: 1 }}>
+      <View style={styles.row}>
+        <Text>{title}</Text>
+        <StatusBar style="auto" />
+      </View>
+    </TouchableOpacity>
   );
 }
 
@@ -17,5 +20,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  row: {
+    width: "100%",
+    flexDirection: "row",
+  },
+  col: {
+    width: "25%",
+    padding: 4,
   },
 });
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -6,29 +6,50 @@ import { FlatList } from "react-native-gesture-handler";
 import Item from "../components/Item";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function Home() {
+export default function Home({ navigation }) {
   const [pageCount, setPageCount] = useState(0);
   const [posts, setPosts] = useState(null);
 
   useEffect(() => {
     Axios.get("https://hn.algolia.com/api/v1/search_by_date?tags=story&page=0")
-      .then((resp) => setPosts(resp.data.hits))
+      .then((resp) => {
+        setPosts(resp.data.hits);
+        console.log(resp.data.hits, "Priya");
+      })
+
       .catch((e) => console.log(e));
 
     const interval = setInterval(() => {
-      setPageCount((pageCount) => pageCount + 1);
+      setPageCount((pageCount = pageCount + 1));
     }, 10000);
     return () => {
       clearInterval(interval);
     };
   }, []);
 
+  useEffect(() => {
+    if (pageCount) {
+      Axios.get(
+        "https://hn.algolia.com/api/v1/search_by_date?tags=story&page=${pageCount}"
+      ).then((resp) => setPosts([...posts, ...resp.data.hits]));
+    }
+  }, [pageCount]);
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
       <FlatList
         data={posts}
-        renderItem={(item) => <Item title={item.title} />}
+        renderItem={(item) => (
+          <Item
+            title={item.title}
+            URL={item.url}
+            created_at={item.created_at}
+            author={item.author}
+            navigation={navigation}
+            data={item}
+          />
+        )}
         keyExtractor={(item) => item.objectID}
       />
     </SafeAreaView>
